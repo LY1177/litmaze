ba60fd6a74c601c5d0502032e72be45ec36aa069
LY1177 <lpgamingg83@gmail.com>
2025-04-25 02:01:34 +0300

    fix: remove duplicate const path declaration

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const session = require('express-session');
 
 const bodyParser = require('body-parser');
-const path = require('path');
+
 
 const app = express();
 const port = 3000;
